f2d4875831770baa8735d8fd64966426f9969e0a
Shivkumar Ople <shivkumarople@gmail.com>
2022-05-05 15:24:27 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module example.com/golangproj
+
+go 1.18
---
+++ rest-api-tutorial/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"log"
+	"encoding/json"
+)
+
+type Article struct{
+	Title string `json:"Title"`
+	Desc string `json:"desc"`
+	Content string `json:"content"`
+}
+
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request){
+	articles := Articles{
+		Article{Title:"Test Title", Desc: "Test Description", Content: "Hello World"},
+	}
+
+	fmt.Println("Endpoint Hit: All articles endpoint")
+	
+	json.NewEncoder(w).Encode(articles)
+}
+
+func homePage(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Homepage Endpoint Hit\n");
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage);
+	http.HandleFunc("/articles", allArticles);
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func main(){
+	handleRequests()
+}
